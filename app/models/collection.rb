a9db458a4802388ccd94b8ed0e7078ac86a10fa3
hamilr2 <hamilr2@rpi.edu>
2011-03-27 20:32:13 -0400

    new tactic

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,12 +1,4 @@
 class Collection < ActiveRecord::Base
   has_and_belongs_to_many :cars
   belongs_to :user
-
-  def candidate_cars
-    Car.find(:all, :order=> "version ASC")
-  end
-
-  def hascar?(car)
-    cars.include?(car)
-  end
 end
